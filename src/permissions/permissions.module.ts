ed86996975e8aa51574c60fee65446dc0036d810
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2025-07-15 20:29:31 +0200

    feat: add profile and permission request endpoints

--- src/permissions/permissions.module.ts
+++ src/permissions/permissions.module.ts
@@ -3,9 +3,10 @@ import { PermissionsService } from './permissions.service';
 import { PermissionsController } from './permissions.controller';
 import { PermissionsRepository } from './permissions.repository';
 import { RolesModule } from 'src/roles/roles.module';
+import { TypedEventEmitterModule } from 'src/event-emitter/event-emitter.module';
 
 @Module({
-  imports: [RolesModule],
+  imports: [RolesModule, TypedEventEmitterModule],
   controllers: [PermissionsController],
   providers: [PermissionsService, PermissionsRepository],
   exports: [PermissionsService],
