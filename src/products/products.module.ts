b03589bd54957a6b04fa1a03d14d1721ade38ce1
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2025-07-20 20:14:57 +0200

    Add product history service with event emitter

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,9 +3,10 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { ProductsRepository } from './products.repository';
 import { FileRepositoryModule } from 'src/file-repository/file-repository.module';
+import { TypedEventEmitterModule } from 'src/event-emitter/event-emitter.module';
 
 @Module({
-  imports: [FileRepositoryModule],
+  imports: [FileRepositoryModule, TypedEventEmitterModule],
   controllers: [ProductsController],
   providers: [ProductsService, ProductsRepository],
   exports: [ProductsService, ProductsRepository],
