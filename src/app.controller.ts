a263738eb90fbd24cc7b13a8cbd5171eac995ae4
rechbe200163 <rechberger.michael@aol.com>
2026-02-19 16:27:09 +0100

    refactor: clean up AppController by removing unused imports and updating ping response

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,25 +1,10 @@
-import {
-  Controller,
-  Get,
-  Request,
-  Post,
-  UseGuards,
-  UseInterceptors,
-  Req,
-  UploadedFile,
-  Body,
-} from '@nestjs/common';
-import { ApiConsumes } from '@nestjs/swagger';
-import { editFileName, imageFileFilter } from 'lib/utils/file-upload-util';
-import { diskStorage } from 'multer';
-import { SingleFileDto } from './products/dto/single-file-dto';
-import { fileMapper } from 'lib/utils/file-mappter';
+import { Controller } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   constructor() {}
 
   ping() {
-    return 'pong';
+    return 200;
   }
 }
