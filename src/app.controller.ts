0c0c394596415f1a5724167af1e2d543c755c78d
rechbe200163 <rechberger.michael@aol.com>
2026-02-20 09:32:44 +0100

    feat: add HTTP status code to health check endpoint and update imports in AppService

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   constructor() {}
 
   @Get('/health')
+  @HttpCode(HttpStatus.OK)
   healthCheck(): { status: string } {
     return { status: 'ok' };
   }
