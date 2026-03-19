29eaad4891012e457ee97dcc5bc2f8a30ef47801
grabne20054 <grabne20054@gmail.com>
2026-03-19 16:43:42 +0100

    add latitude, longitude to save in db

--- src/addresses/addresses.service.ts
+++ src/addresses/addresses.service.ts
@@ -5,12 +5,39 @@ import { UpdateAddressDto } from 'prisma/src/generated/dto/update-address.dto';
 import { PagingResultDto } from 'lib/dto/genericPagingResultDto';
 import { AddressDto } from 'prisma/src/generated/dto/address.dto';
 
+const geocoding = require('@aashari/nodejs-geocoding');
+
 @Injectable()
 export class AddressesService {
   constructor(private readonly addressesRepository: AddressesRepository) {}
 
-  create(createAddressDto: CreateAddressDto): Promise<AddressDto> {
-    return this.addressesRepository.create(createAddressDto);
+  async create(createAddressDto: CreateAddressDto): Promise<AddressDto> {
+    let latitude_db: number | null = null;
+    let longitude_db: number | null = null;
+    
+    const fullAddress = `${createAddressDto.streetNumber} ${createAddressDto.streetName}, ${createAddressDto.city}, ${createAddressDto.state}, ${createAddressDto.postCode}, ${createAddressDto.country}`;
+    try {
+      const location = (await geocoding.encode(fullAddress)) as Array<{ latitude: number; longitude: number }>;
+      if (location && location.length > 0) {
+        const { latitude, longitude } = location[0];
+        latitude_db = latitude;
+        longitude_db = longitude;
+      }
+    } catch (err: unknown) {
+      console.error('Geocoding error:', err);
+    }
+    const dbAddress = {
+      city: createAddressDto.city,
+      country: createAddressDto.country,
+      postCode: createAddressDto.postCode,
+      state: createAddressDto.state,
+      streetName: createAddressDto.streetName,
+      streetNumber: createAddressDto.streetNumber,
+      latitude: latitude_db,
+      longitude: longitude_db
+    };
+    console.log('Creating address with data:', dbAddress);
+    return await this.addressesRepository.create(dbAddress);
   }
 
   findAllPaging(
